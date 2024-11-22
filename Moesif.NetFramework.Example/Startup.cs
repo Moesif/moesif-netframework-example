472406e3a48741769809aeff0268b9d4c4215b40
Keyur <keyur.kv@gmail.com>
2024-11-22 05:22:53 +0000

    Fixed test and included step to test large payload via api call /product/>25 (this is size in KB)

--- Moesif.NetFramework.Example/Startup.cs
+++ Moesif.NetFramework.Example/Startup.cs
@@ -13,7 +13,7 @@ namespace Moesif.NetFramework.Example
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-            app.Use<MoesifMiddleware>(MoesifOptions.moesifOptions);
+            app.Use<MoesifMiddleware>(MoesifOptions.GetMoesifOptions());
         }
     }
 }
